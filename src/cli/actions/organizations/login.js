f16640ed5b7ac930877d3b62cd7e389f4a5c52c7
Scott Motte <mot@mot.la>
2024-09-23 09:39:14 -0700

    update messaging

--- src/cli/actions/organizations/login.js
+++ src/cli/actions/organizations/login.js
@@ -31,7 +31,7 @@ async function login () {
 
       logger.success(`✔ ${input.organization} set`)
     } else {
-      logger.error('Create an organization [dotenvx pro settings orgnew] or join one [dotenvx pro organizations join].')
+      logger.error('Create an organization [dotenvx pro settings orgnew] or join one [dotenvx pro settings orgjoin].')
     }
   } catch (error) {
     if (error.message) {
