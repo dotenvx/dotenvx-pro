22d4ba7e01941331f5da6cc694a29c63d1391fc5
Scott Motte <mot@mot.la>
2024-10-06 08:11:55 -0700

    ls now considers what lives in the db

--- src/cli/actions/ls.js
+++ src/cli/actions/ls.js
@@ -1,26 +1,53 @@
 const treeify = require('object-treeify')
 
-const { logger } = require('@dotenvx/dotenvx')
+const { logger, getColor } = require('@dotenvx/dotenvx')
 
 const dotenvx = require('@dotenvx/dotenvx')
 const ArrayToTree = require('./../../lib/helpers/arrayToTree')
 
+const DbKeypair = require('./../../lib/services/dbKeypair')
+
+const notSyncedIcon = getColor('red')('✖')
+const syncedIcon = getColor('green')('✔')
+
 function ls (directory) {
-  // debug args
-  logger.debug(`directory: ${directory}`)
+  try {
+    // debug args
+    logger.debug(`directory: ${directory}`)
 
-  const options = this.opts()
-  logger.debug(`options: ${JSON.stringify(options)}`)
+    const options = this.opts()
+    logger.debug(`options: ${JSON.stringify(options)}`)
 
-  const filepaths = dotenvx.ls(directory, options.envFile, options.excludeEnvFile)
-  logger.debug(`filepaths: ${JSON.stringify(filepaths)}`)
+    const filepaths = dotenvx.ls(directory, options.envFile, options.excludeEnvFile)
+    logger.debug(`filepaths: ${JSON.stringify(filepaths)}`)
 
-  const filepathsWithCheckmarks = filepaths.map(filepath => `${filepath} ✔`)
+    // if present in db keypairs then data has been pushed/synced
+    const filepathsWithCheckmarks = filepaths.map(filepath => {
+      // short-circuit for .env.keys file
+      if (filepath.endsWith('.env.keys')) {
+        return `${filepath}`
+      }
 
-  const tree = new ArrayToTree(filepathsWithCheckmarks).run()
-  logger.debug(`tree: ${JSON.stringify(tree)}`)
+      const keypair = new DbKeypair(filepath).run()
+      if (Object.keys(keypair).length > 0) {
+        return `${filepath} ${syncedIcon}`
+      } else {
+        return `${filepath} ${notSyncedIcon}`
+      }
+    })
 
-  logger.info(treeify(tree))
+    const tree = new ArrayToTree(filepathsWithCheckmarks).run()
+    logger.debug(`tree: ${JSON.stringify(tree)}`)
+
+    logger.info(treeify(tree))
+  } catch (error) {
+    if (error.message) {
+      console.error(error.message)
+    } else {
+      console.error(error)
+    }
+    process.exit(1)
+  }
 }
 
 module.exports = ls
--- src/lib/services/keypair.js
+++ src/lib/services/keypair.js
@@ -1,119 +1,25 @@
 const { keypair } = require('@dotenvx/dotenvx')
 
-const gitUrl = require('./../helpers/gitUrl')
-const validateGit = require('./../helpers/validateGit')
-const extractSlug = require('./../helpers/extractSlug')
-const extractUsernameName = require('./../helpers/extractUsernameName')
-
-// db
-const User = require('./../../db/user')
-const Organization = require('./../../db/organization')
+const DbKeypair = require('./dbKeypair')
 
 class Keypair {
   constructor (envFile = '.env', key = undefined) {
     this.envFile = envFile
     this.key = key
 
-    this.user = new User()
-
     this._mem = {}
   }
 
   run () {
-    validateGit()
+    const keypairs = keypair(this.envFile)
+    const dbKeypairs = new DbKeypair(this.envFile).run()
+    const out = { ...keypairs, ...dbKeypairs } // db keypairs win
 
-    // start with local keypairs
-    const keypairs = keypair(this.envFile, this.key)
-
-    for (const envFilepath of this._envFilepaths()) {
-      const envFileId = this.lookups()[`lookup/envFileIdByUsernameNameFilepath/${this.usernameName()}/${envFilepath}`]
-      if (!envFileId) {
-        continue
-      }
-
-      const publicKeyName = this.organization().store.get(`r/${this.repositoryId()}/e/${envFileId}/pkn`)
-      const publicKey = this.organization().store.get(`r/${this.repositoryId()}/e/${envFileId}/pk/1`)
-      if (publicKeyName && publicKey) {
-        keypairs[publicKeyName] = publicKey
-      }
-
-      const privateKeyName = this.organization().store.get(`r/${this.repositoryId()}/e/${envFileId}/ekn`)
-      const privateKeyEncrypted = this.organization().store.get(`r/${this.repositoryId()}/e/${envFileId}/ek/1`)
-      if (privateKeyName && privateKeyEncrypted) {
-        const privateKey = this.organization().decrypt(privateKeyEncrypted)
-        keypairs[privateKeyName] = privateKey
-      }
+    if (this.key) {
+      return out[this.key]
+    } else {
+      return out
     }
-
-    return keypairs
-  }
-
-  organization () {
-    if (this._mem.organization) {
-      return this._mem.organization
-    }
-
-    const result = new Organization(this.organizationId())
-    this._mem.organization = result
-    return result
-  }
-
-  usernameName () {
-    if (this._mem.usernameName) {
-      return this._mem.usernameName
-    }
-
-    const result = extractUsernameName(gitUrl())
-    this._mem.usernameName = result
-    return result
-  }
-
-  slug () {
-    if (this._mem.slug) {
-      return this._mem.slug
-    }
-
-    const result = extractSlug(this.usernameName())
-    this._mem.slug = result
-    return result
-  }
-
-  lookups () {
-    if (this._mem.lookups) {
-      return this._mem.lookups
-    }
-
-    const result = this.user.lookups()
-    this._mem.lookups = result
-    return result
-  }
-
-  organizationId () {
-    const id = this.lookups()[`lookup/organizationIdBySlug/${this.slug()}`]
-
-    if (!id) {
-      throw new Error(`oops, can't find organization [@${this.slug()}]. did you join it? [dotenvx pro settings orgjoin]`)
-    }
-
-    return id
-  }
-
-  repositoryId () {
-    const id = this.lookups()[`lookup/repositoryIdByUsernameName/${this.usernameName()}`]
-
-    if (!id) {
-      throw new Error(`oops, can't find project [@${this.usernameName()}]. did you push it? [dotenvx pro push]`)
-    }
-
-    return id
-  }
-
-  _envFilepaths () {
-    if (!Array.isArray(this.envFile)) {
-      return [this.envFile]
-    }
-
-    return this.envFile
   }
 }
 
