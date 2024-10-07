804e5a4e6b474241a5ac6fcf7a13e5a600bd16fc
Scott Motte <mot@mot.la>
2024-10-06 17:40:37 -0700

    move push to a service largely

--- src/cli/actions/push.js
+++ src/cli/actions/push.js
@@ -5,8 +5,6 @@ const { logger } = require('@dotenvx/dotenvx')
 const current = require('./../../db/current')
 const User = require('./../../db/user')
 
-const isGitRepo = require('./../../lib/helpers/isGitRepo')
-const isGithub = require('./../../lib/helpers/isGithub')
 const gitUrl = require('./../../lib/helpers/gitUrl')
 const gitRoot = require('./../../lib/helpers/gitRoot')
 const extractUsernameName = require('./../../lib/helpers/extractUsernameName')
@@ -17,25 +15,18 @@ const { createSpinner } = require('./../../lib/helpers/createSpinner')
 // api calls
 const PostPush = require('./../../lib/api/postPush')
 
+// services
+const Push = require('./../../lib/services/push')
+
 // SyncOrganization
 const SyncOrganization = require('./../../lib/services/syncOrganization')
 const Keypair = require('./../../lib/services/keypair')
 
 const spinner = createSpinner('pushing')
 
-function _envFilepaths (directory, envFile) {
-  if (!Array.isArray(envFile)) {
-    return [path.join(directory, envFile)]
-  }
-
-  return envFile.map(file => path.join(directory, file))
-}
-
 async function push (directory) {
   try {
     spinner.start()
-
-    directory = forgivingDirectory(directory)
 
     // debug args
     logger.debug(`directory: ${directory}`)
@@ -44,100 +35,9 @@ async function push (directory) {
     const options = this.opts()
     logger.debug(`options: ${JSON.stringify(options)}`)
 
-    // must be a git repo
-    if (!isGitRepo()) {
-      spinner.fail('oops, must be a git repository')
-      logger.help('? create one with [git init .]')
-      process.exit(1)
-    }
-
-    // must be a git root
-    const gitroot = gitRoot()
-    if (!gitroot) {
-      spinner.fail('oops, could not determine git repository\'s root')
-      logger.help('? create one with [git init .]')
-      process.exit(1)
-    }
-
-    // must have a remote origin url
-    const giturl = gitUrl()
-    if (!giturl) {
-      spinner.fail('oops, must have a remote origin (git remote -v)')
-      logger.help('? create it at [github.com/new] and then run [git remote add origin git@github.com:username/repository.git]')
-      process.exit(1)
-    }
-
-    // must be a github remote
-    if (!isGithub(giturl)) {
-      spinner.fail('oops, must be a github.com remote origin (git remote -v)')
-      logger.help('? create it at [github.com/new] and then run [git remote add origin git@github.com:username/repository.git]')
-      logger.help2('ℹ need support for other origins? [please tell us](https://github.com/dotenvx/dotenvx/issues)')
-      process.exit(1)
-    }
-
-    // find organization locally
-    const usernameName = extractUsernameName(giturl)
-    const slug = extractSlug(usernameName)
-    const user = new User()
-    const lookups = user.lookups()
-    const organizationId = lookups[`lookup/organizationIdBySlug/${slug}`]
-    if (!organizationId) {
-      spinner.fail(`oops, can't find organization [@${slug}]`)
-      logger.help('? try running [dotenvx pro sync] or joining organization [dotenvx pro settings orgjoin]')
-      process.exit(1)
-    }
-
-    // sync org
-    const organization = await new SyncOrganization(options.hostname, current.token(), organizationId).run()
-
-    // check for publicKey
-    if (!organization.publicKey()) {
-      spinner.fail(`oops, can't find orgpublickey for [@${slug}]`)
-      logger.help('? try running [dotenvx pro sync]')
-      process.exit(1)
-    }
-
-    // -f .env,etc
-    const envFilepaths = _envFilepaths(directory, options.envFile)
-    for (const envFilepath of envFilepaths) {
-      const filepath = path.resolve(envFilepath)
-
-      // file must exist
-      if (!fs.existsSync(filepath)) {
-        spinner.fail(`oops, missing ${envFilepath} file (${filepath})`)
-        logger.help(`? add one with [echo "HELLO=World" > ${envFilepath}]`)
-        process.exit(1)
-      }
-
-      // get keypairs
-      const keypairs = new Keypair(envFilepath).run()
-
-      // publicKey must exist
-      const publicKeyName = Object.keys(keypairs).find(key => key.startsWith('DOTENV_PUBLIC_KEY'))
-      const publicKey = keypairs[publicKeyName]
-      if (!publicKey) {
-        spinner.fail(`oops, could not locate ${publicKeyName}`)
-        logger.help(`? generate ${publicKeyName} (.env.keys) with [dotenvx encrypt]`)
-        process.exit(1)
-      }
-
-      // privateKey
-      const privateKeyName = Object.keys(keypairs).find(key => key.startsWith('DOTENV_PRIVATE_KEY'))
-      const privateKey = keypairs[privateKeyName]
-      const privateKeyEncryptedWithOrganizationPublicKey = organization.encrypt(privateKey)
-
-      // filepath
-      const relativeFilepath = path.relative(gitroot, path.join(process.cwd(), directory, envFilepath)).replace(/\\/g, '/') // smartly determine path/to/.env file from repository root - where user is cd-ed inside a folder or at repo root
-
-      // text
-      const text = fs.readFileSync(filepath, 'utf8')
-
-      await new PostPush(options.hostname, current.token(), 'github', organization.publicKey(), usernameName, relativeFilepath, publicKeyName, privateKeyName, publicKey, privateKeyEncryptedWithOrganizationPublicKey, text).run()
-
-      // sync org
-      await new SyncOrganization(options.hostname, current.token(), organizationId).run()
-
-      spinner.succeed(`pushed (${relativeFilepath})`)
+    const pushedFilepaths = await new Push(options.hostname, options.envFile).run()
+    for (const filepath of pushedFilepaths) {
+      spinner.succeed(`pushed (${filepath})`)
     }
   } catch (error) {
     if (error.message) {
