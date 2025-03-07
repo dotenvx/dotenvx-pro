79c43b3deef714b0193240391bf5398dae9894b1
Scott Motte <mot@mot.la>
2025-03-07 14:53:47 -0800

    adds `storetree` settings option

---
+++ src/cli/actions/settings/storetree.js
@@ -0,0 +1,32 @@
+const path = require('path')
+const { fdir: Fdir } = require('fdir')
+const treeify = require('object-treeify')
+
+const current = require('./../../../db/current')
+const ArrayToTree = require('./../../../lib/helpers/arrayToTree')
+
+function filepaths(directory) {
+  return new Fdir()
+    .withRelativePaths()
+    .crawl(directory)
+    .sync()
+}
+
+function storetree () {
+  try {
+    const _storepath = current.configPath()
+    if (_storepath && _storepath.length > 1) {
+      const directory = path.dirname(_storepath)
+      const tree = new ArrayToTree(filepaths(directory)).run()
+      console.log(treeify(tree))
+    } else {
+      console.error('missing storepath. Try running [dotenvx pro login].')
+      process.exit(1)
+    }
+  } catch (error) {
+    console.error(error.message)
+    process.exit(1)
+  }
+}
+
+module.exports = storetree
